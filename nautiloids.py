787561f0141c3a1384dae8d4e6978e499f37b603
matthew.bandel <mcmbandel@gmail.com>
2021-05-27 20:47:44 -0700

    working on fadeout

--- nautiloids.py
+++ nautiloids.py
@@ -1,6 +1,10 @@
 # import reload
 from importlib import reload
 
+# import math
+from math import exp
+import numpy
+
 # import scipy to read wavfiles
 from scipy.io import wavfile
 
@@ -19,12 +23,24 @@ duo = [float(stereo[1]) for stereo in data]
 start = 0
 finish = 200000
 
-
-
 # plot
 pyplot.clf()
 pyplot.plot(times[start:finish], mono[start:finish], 'b--')
 pyplot.plot(times[start:finish], duo[start:finish], 'g--')
 pyplot.savefig('plots/testo.png')
 
-print(frequency, len(data))
+# apply fade
+chunk = int(0.1 * len(data))
+denouement = data[:chunk]
+
+# generate fade
+fade = [exp(-(index ** 2) / (chunk ** 2)) for index, _ in enumerate(denouement)]
+fade = [[value, value] for value in fade]
+denouement = denouement * fade
+denouement = denouement.astype('int16')
+
+# concatenate
+combo = numpy.concatenate([data, denouement], axis=0)
+
+# rewrite file
+wavfile.write('gems/Lichenthrope_fade.wav', frequency, combo)
